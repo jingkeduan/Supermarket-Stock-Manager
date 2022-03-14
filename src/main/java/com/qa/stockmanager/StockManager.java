db0fd567addad5ce8982876bc9ad64604b5c1d49
jingkeduan <jingke.d@hotmail.com>
2022-03-14 15:03:35 +0000

    set up initial project structure

---
+++ src/main/java/com/qa/stockmanager/StockManager.java
@@ -0,0 +1,13 @@
+package com.qa.stockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManager.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/manager/DfeSw12FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeSw12FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
